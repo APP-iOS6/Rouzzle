7510a49836734de80503c93b240e8ac572ccf2b4
gadisom <jeongwon.kim@gmail.com>
2024-11-01 15:16:11 +0900

    Chore: SwiftLint 설정

---
+++ Rouzzle/Rouzzle/RouzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouzzleApp.swift
+//  Rouzzle
+//
+//  Created by 김정원 on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RouzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
