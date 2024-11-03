e116d1a7286e8df570cb591aaea0adef97e20611
kim dong kyung <kdkd422@naver.com>
2024-11-03 23:07:26 +0900

    Feat 로그인 뷰모델 추가

--- Rouzzle/Rouzzle/Features/Login/ViewModel/LoginViewModel.swift
+++ Rouzzle/Rouzzle/Features/Login/ViewModel/LoginViewModel.swift
@@ -4,3 +4,34 @@
 //
 //  Created by 김정원 on 11/2/24.
 //
+import Foundation
+import Observation
+import Factory
+import AuthenticationServices
+
+@Observable
+class LoginViewModel {
+    
+    @ObservationIgnored
+    @Injected(\.authService) private var authService
+    
+    enum Action {
+        case google
+        case kakao
+        case appleLogin(ASAuthorizationAppleIDRequest)
+        case appleLoginCompletion(Result<ASAuthorization, Error>)
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .google:
+            return
+        case .kakao:
+            return
+        case .appleLogin(_):
+            return
+        case .appleLoginCompletion(_):
+            return
+        }
+    }
+}
