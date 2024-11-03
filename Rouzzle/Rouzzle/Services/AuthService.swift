38c4590a875f9035d5476faa4bdbb05ecc77acaf
kim dong kyung <kdkd422@naver.com>
2024-11-04 02:41:59 +0900

    Feat AuthService 카카오톡 로그인 함수 작성

--- Rouzzle/Rouzzle/Services/AuthService.swift
+++ Rouzzle/Rouzzle/Services/AuthService.swift
@@ -9,12 +9,17 @@ import Foundation
 import FirebaseCore
 import FirebaseAuth
 import GoogleSignIn
+import KakaoSDKAuth
+import KakaoSDKUser
 
 protocol AuthServiceType {
     func signInWithGoogle() async -> Result<String, Error>
+    func signInWithKakao() async -> Result<String, Error>
 }
 
+// MARK: 구글 로그인 구현
 class AuthService: AuthServiceType {
+    @MainActor
     func signInWithGoogle() async -> Result<String, Error> {
         
         guard let clientId = FirebaseApp.app()?.options.clientID else {
@@ -24,8 +29,8 @@ class AuthService: AuthServiceType {
         let config = GIDConfiguration(clientID: clientId)
         GIDSignIn.sharedInstance.configuration = config
         
-        guard let windowScene = await UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = await windowScene.windows.first,
-              let rootViewController = await window.rootViewController else {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = windowScene.windows.first,
+              let rootViewController = window.rootViewController else {
             return .failure(AuthError.invalidate)
         }
         
@@ -50,12 +55,110 @@ class AuthService: AuthServiceType {
     }
 }
 
+// MARK: 카카오 로그인 구현을 위한 extension
+extension AuthService {
+    @MainActor
+    func signInWithKakao() async -> Result<String, Error> {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            do {
+                let oauthToken = try await loginWithKakaoTalkAsync()
+                _ = try await loginWithEmailPermissionAsync()
+                
+                guard let idToken = oauthToken.idToken else {
+                    return .failure(AuthError.tokenError)
+                }
+                
+                let accessToken = oauthToken.accessToken
+                
+                let crendential = OAuthProvider.credential(providerID: .custom("oidc.oidc.kakao"), idToken: idToken, accessToken: accessToken)
+                
+                return try await authenticationUserWithFirebase(credential: crendential)
+                
+            } catch {
+                return .failure(AuthError.clientIdError)
+            }
+        } else {
+            do {
+                let oauthToken = try await loginWithKakaoTalkAccountAsync()
+                _ = try await loginWithEmailPermissionAsync()
+                
+                guard let idToken = oauthToken.idToken else {
+                    return .failure(AuthError.tokenError)
+                }
+                
+                let accessToken = oauthToken.accessToken
+                
+                let credential = OAuthProvider.credential(providerID: .custom("oidc.oidc.kakao"), idToken: idToken, rawNonce: randomNonceString(), accessToken: accessToken)
+
+                return try await authenticationUserWithFirebase(credential: credential)
+                
+            } catch {
+                print(error.localizedDescription)
+                return .failure(AuthError.clientIdError)
+            }
+        }
+    }
+    
+    @MainActor
+    private func loginWithKakaoTalkAsync() async throws -> OAuthToken {
+        // 비동기 코드가 아닌 것을 비동기로 실행할 수 있도록 하는 함수, 오류 반환 가능
+        try await withCheckedThrowingContinuation { continuation in
+            UserApi.shared.loginWithKakaoTalk { oauthToken, error in
+                if let error {
+                    print(error.localizedDescription)
+                    continuation.resume(throwing: error)
+                }
+                
+                if let oauthToken {
+                    continuation.resume(returning: oauthToken)
+                }
+            }
+        }
+    }
+    
+    @MainActor
+    private func loginWithKakaoTalkAccountAsync() async throws -> OAuthToken {
+        // 비동기 코드가 아닌 것을 비동기로 실행할 수 있도록 하는 함수, 오류 반환 가능
+        try await withCheckedThrowingContinuation { continuation in
+            UserApi.shared.loginWithKakaoAccount { oauthToken, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                }
+                
+                if let oauthToken {
+                    continuation.resume(returning: oauthToken)
+                }
+            }
+        }
+    }
+    
+    @MainActor
+    private func loginWithEmailPermissionAsync() async throws -> OAuthToken {
+        try await withCheckedThrowingContinuation { continuation in
+            let scopes: [String] = ["openid", "account_email"]
+            
+            UserApi.shared.loginWithKakaoAccount(scopes: scopes) { oauthToken, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                }
+                
+                if let oauthToken {
+                    continuation.resume(returning: oauthToken)
+                }
+            }
+        }
+    }
+    
+}
+
+// MARK: 소셜 로그인으로 부처 credential을 받아 FirebaseAuth 로그인 하는 Extension
 extension AuthService {
     private func authenticationUserWithFirebase(credential: AuthCredential) async throws -> Result<String, Error> {
         do {
             let result = try await Auth.auth().signIn(with: credential)
             return .success(result.user.uid)
         } catch {
+            print("여기서 오류남")
             throw AuthError.signInError
         }
     }
