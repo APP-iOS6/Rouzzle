6787e92c8964c6af448f035b9e52ccefb659fc78
kim dong kyung <kdkd422@naver.com>
2024-11-04 02:36:30 +0900

    Feat 카카오 로그인 SDK init 추가 및 Delegate 카카오 함수 추가

--- Rouzzle/Rouzzle/App/RouzzleApp.swift
+++ Rouzzle/Rouzzle/App/RouzzleApp.swift
@@ -7,10 +7,19 @@
 
 import SwiftUI
 import FirebaseCore
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct RouzzleApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        let kakaoAppkey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        // Kakao SDK 초기화
+        KakaoSDK.initSDK(appKey: kakaoAppkey as? String ?? "")
+        print("appKey: \(kakaoAppkey)")
+    }
     
     var body: some Scene {
         WindowGroup {
@@ -20,9 +29,30 @@ struct RouzzleApp: App {
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    // 카카오 로그인 open url 받기 위해
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        
+        return false
+    }
+    
+    // SceneDelegate 뭘 쓸지 정해줘야 함
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        
+        sceneConfiguration.delegateClass = AppSceneDelegate.self
+        
+        return sceneConfiguration
+    }
 }
