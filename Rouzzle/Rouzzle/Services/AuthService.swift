b1cd03d0d6a363fdd64cdd5e7e91ebf8e7eafa85
kim dong kyung <kdkd422@naver.com>
2024-11-03 23:07:50 +0900

    Feaet AuthService 추가

--- Rouzzle/Rouzzle/Services/AuthService.swift
+++ Rouzzle/Rouzzle/Services/AuthService.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
+
+protocol AuthServiceType {
+    func signInWithGoogle() async -> Result<String, Error>
+}
+
+class AuthService: AuthServiceType {
+    func signInWithGoogle() async -> Result<String, Error> {
+        
+        guard let clientId = FirebaseApp.app()?.options.clientID else {
+            return (.failure(AuthError.clientIdError))
+        }
+        
+        let config = GIDConfiguration(clientID: clientId)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        guard let windowScene = await UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = await windowScene.windows.first,
+              let rootViewController = await window.rootViewController else {
+            return .failure(AuthError.invalidate)
+        }
+        
+        do {
+            return .success("")
+        } catch {
+            return .failure(error)
+        }
+    }
+}
+
