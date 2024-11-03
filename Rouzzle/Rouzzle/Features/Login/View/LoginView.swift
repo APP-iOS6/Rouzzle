3823ae6cac4d2352302b3308f568e077fdf3c350
kim dong kyung <kdkd422@naver.com>
2024-11-03 23:07:08 +0900

    Feat 로그인 소셜 로그인 버튼 뷰 추가

--- Rouzzle/Rouzzle/Features/Login/View/LoginView.swift
+++ Rouzzle/Rouzzle/Features/Login/View/LoginView.swift
@@ -6,14 +6,66 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct LoginView: View {
     var body: some View {
-        VStack {
-            Text("Hello")
-                .padding()
+        VStack(alignment: .center, spacing: 17) {
+            
+            Spacer()
+            
+            // MARK: 카카오 로그인 버튼
+            Button {
+                
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundStyle(.kakaocolor)
+                        .frame(maxWidth: .infinity, maxHeight: 60)
+                    HStack {
+                        Image(.kakaologo)
+                            .padding(.trailing)
+                        Text("카카오로 시작하기")
+                            .foregroundStyle(.black)
+                            .bold()
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            // MARK: 애플 로그인 버튼
+            HStack {
+                Image(.applelogo)
+                    .padding(.trailing)
+                Text("Apple로 계속하기")
+                    .bold()
+            }
+            .overlay {
+                SignInWithAppleButton(.continue) { request in
+                    print(request)
+                } onCompletion: { result in
+                    print(result)
+                }
+                .blendMode(.overlay)
+            }
+            .modifier(SignUpButtonModifier())
+            
+            // MARK: 구글 로그인 버튼
+            Button {
+                
+            } label: {
+                HStack {
+                    Image(.googlelogo)
+                        .padding(.trailing)
+                    Text("Google로 계속하기")
+                        .foregroundStyle(.black)
+                        .bold()
+                }
+            }
+            .modifier(SignUpButtonModifier())
         }
-        .padding()
+        .padding(.horizontal)
+        .padding(.vertical, 32)
     }
 }
 
