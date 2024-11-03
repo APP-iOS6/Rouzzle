2ccc8fc057a9cf6502470d41c67627af12b273b4
kim dong kyung <kdkd422@naver.com>
2024-11-04 02:35:58 +0900

    Chore 카카오톡 로그인을 위해 AppSceneDelegate 추가

--- Rouzzle/Rouzzle/App/AppSceneDelegate.swift
+++ Rouzzle/Rouzzle/App/AppSceneDelegate.swift
@@ -5,4 +5,15 @@
 //  Created by 김동경 on 11/4/24.
 //
 
-import Foundation
+import UIKit
+import KakaoSDKAuth
+
+class AppSceneDelegate: UIResponder, UIWindowSceneDelegate {
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if AuthApi.isKakaoTalkLoginUrl(url) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
+        }
+    }
+}
