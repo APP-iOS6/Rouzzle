4f81c4e57d976dee6fa247693046739fca4e45d8
kim dong kyung <kdkd422@naver.com>
2024-11-05 00:40:18 +0900

    Feat 앱 분기 나뉘는 LaunchView 추가

--- Rouzzle/Rouzzle/Features/Launch/LaunchView.swift
+++ Rouzzle/Rouzzle/Features/Launch/LaunchView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct LaunchView: View {
+    
+    @Environment(AuthStore.self) var authStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            switch authStore.authState {
+            case .splash:
+                Text("ROUZZLE")
+                    .font(.haloDek48)
+                    .tracking(12)
+                    .foregroundStyle(.button)
+            case .login:
+                EmptyView()
+            case .signup:
+                EmptyView()
+            case .authenticated:
+                EmptyView()
+            }
+        }
+        .animation(.smooth, value: authStore.authState)
     }
 }
 
 #Preview {
     LaunchView()
+        .environment(AuthStore())
 }
