dfe17843ae074a3abd8f1563d94d0653f34b9d24
kim dong kyung <kdkd422@naver.com>
2024-11-05 02:37:17 +0900

    Feat 회원가입뷰 추가

--- Rouzzle/Rouzzle/Features/SignUp/View/SignUpView.swift
+++ Rouzzle/Rouzzle/Features/SignUp/View/SignUpView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @Environment(AuthStore.self) private var authStore
+    private let viewModel: SignUpViewModel = SignUpViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        @Bindable var vm = viewModel
+        
+        VStack {
+            Text("ROUZZLE")
+                .font(.haloDek48)
+                .tracking(12)
+                .foregroundStyle(.button)
+                .transition(.opacity)
+                .padding(.bottom, 64)
+            
+            Text("닉네임")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .bold()
+                .padding([.top, .horizontal])
+            
+            TextField("똑똑한 컴퓨터", text: $vm.user.name)
+                .modifier(StrokeTextFieldModifier())
+                .padding()
+            
+            RouzzleButton(buttonType: .start) {
+                Task {
+                    await viewModel.uploadUserData()
+                }
+            }
+        }
+        .overlay {
+            if viewModel.loadState == .loading {
+                ProgressView()
+            }
+        }
+        .onChange(of: viewModel.loadState) { _, newValue in
+            if newValue == .completed {
+                authStore.performLogin()
+            }
+        }
     }
 }
 
 #Preview {
     SignUpView()
+        .environment(AuthStore())
 }
