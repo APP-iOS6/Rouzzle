4caaef2bae10673e595f09b4b42517da4436c8e3
kim dong kyung <kdkd422@naver.com>
2024-11-05 02:35:42 +0900

    Feat 유저에 대한 비즈니스 로직을 수행하는 UserService 작성

--- Rouzzle/Rouzzle/Services/UserService.swift
+++ Rouzzle/Rouzzle/Services/UserService.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+protocol UserServiceType {
+    func uploadUserData(_ userUid: String, user: RoutineUser) async -> Result<Void, Error>
+}
+
+class UserService: UserServiceType {
+    
+    private let db = Firestore.firestore()
+    
+    /// 유저 데이터를 FireStore User컬렉션에 등록하는 함수
+    func uploadUserData(_ userUid: String, user: RoutineUser) async -> Result<Void, Error> {
+        do {
+            let userEncodeData = try Firestore.Encoder().encode(user)
+            try await self.db.collection("User").document(userUid).setData(userEncodeData, merge: true)
+            return .success(())
+        } catch {
+            return .failure(error)
+        }
+    }
+}
