b6a96a106309d5415ea7d41e4eb91ba182bcd914
gadisom <jeongwon.kim@gmail.com>
2024-11-04 11:24:31 +0900

    Feat: Pretendard font 추가

--- Rouzzle/Rouzzle/Utils/Extensions/Font+Extension.swift
+++ Rouzzle/Rouzzle/Utils/Extensions/Font+Extension.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 extension Font {
-    /// bold22
-    static var bold22: Font {
-        return Font.custom("NotoSansKR-Bold", size: 22)
+    /// Bold Fonts
+    static var bold24: Font {
+        return Font.custom("Pretendard-Bold", size: 24)
+    }
+    static var bold20: Font {
+        return Font.custom("Pretendard-Bold", size: 20)
+    }
+    static var bold16: Font {
+        return Font.custom("Pretendard-Bold", size: 16)
+    }
+
+    /// SemiBold Fonts
+    static var semibold24: Font {
+        return Font.custom("Pretendard-SemiBold", size: 24)
+    }
+    static var semibold20: Font {
+        return Font.custom("Pretendard-SemiBold", size: 20)
+    }
+    static var semibold16: Font {
+        return Font.custom("Pretendard-SemiBold", size: 16)
+    }
+    static var semibold12: Font {
+        return Font.custom("Pretendard-SemiBold", size: 12)
+    }
+    static var semibold8: Font {
+        return Font.custom("Pretendard-SemiBold", size: 8)
+    }
+
+    /// Regular Fonts
+    static var regular24: Font {
+        return Font.custom("Pretendard-Regular", size: 24)
+    }
+    static var regular20: Font {
+        return Font.custom("Pretendard-Regular", size: 20)
+    }
+    static var regular16: Font {
+        return Font.custom("Pretendard-Regular", size: 16)
     }
     
+    static var regular14: Font {
+        return Font.custom("Pretendard-Regular", size: 14)
+    }
+    
+    static var regular12: Font {
+        return Font.custom("Pretendard-Regular", size: 12)
+    }
+    static var regular10: Font {
+        return Font.custom("Pretendard-Regular", size: 10)
+    }
 }
