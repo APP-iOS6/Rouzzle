8039c41bd502c48403fe7fb074cc3b9abf3ff4c4
kim dong kyung <kdkd422@naver.com>
2024-11-05 02:30:29 +0900

    Feat 테두리 텍스트필드 모디파이어 추가

--- Rouzzle/Rouzzle/Utils/Modifier/StrokeTextFieldModifier.swift
+++ Rouzzle/Rouzzle/Utils/Modifier/StrokeTextFieldModifier.swift
@@ -5,4 +5,19 @@
 //  Created by 김동경 on 11/5/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct StrokeTextFieldModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .textInputAutocapitalization(.never)
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(.gray, lineWidth: 1)
+            }
+    }
+}
