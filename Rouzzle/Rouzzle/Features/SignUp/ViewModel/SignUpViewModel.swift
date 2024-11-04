f2cb84df750ac7fa2edac20c61af36fa06ded741
kim dong kyung <kdkd422@naver.com>
2024-11-05 02:36:47 +0900

    Feat 회원가입 뷰모델 추가

--- Rouzzle/Rouzzle/Features/SignUp/ViewModel/SignUpViewModel.swift
+++ Rouzzle/Rouzzle/Features/SignUp/ViewModel/SignUpViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+import Observation
+import Factory
+
+@Observable
+class SignUpViewModel {
+    
+    @ObservationIgnored
+    @Injected(\.userService) private var userService
+    
+    var loadState: LoadState = .none
+    var user: RoutineUser = RoutineUser(name: "")
+    var errorMessage: String? // 토스트 메시지 띄울 메시지 내용
+    
+    @MainActor
+    func uploadUserData() async {
+        
+        guard !user.name.isEmpty else {
+            errorMessage = "닉네임을 입력해 주세요."
+            return
+        }
+        
+        let userUid = Auth.auth().currentUser?.uid ?? Utils.getDeviceUUID()
+        loadState = .loading
+        
+        switch await userService.uploadUserData(userUid, user: user) {
+        case .success:
+            // 회원가입 성공함
+            loadState = .completed
+        case .failure:
+            // 회원가입 실패함(네트워크 오류 등)
+            loadState = .failed
+        }
+        
+    }
+    
+}
