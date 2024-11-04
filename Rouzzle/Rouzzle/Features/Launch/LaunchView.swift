d8d30b09ff2fada1652e7e0c580bf9acf279281d
kim dong kyung <kdkd422@naver.com>
2024-11-05 00:37:09 +0900

    Chore project.pbxproj

---
+++ Rouzzle/Rouzzle/Features/Launch/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  Rouzzle
+//
+//  Created by 김동경 on 11/4/24.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
---
+++ Rouzzle/Rouzzle/Store/AuthStore.swift
@@ -0,0 +1,8 @@
+//
+//  AuthStore.swift
+//  Rouzzle
+//
+//  Created by 김동경 on 11/4/24.
+//
+
+import Foundation
