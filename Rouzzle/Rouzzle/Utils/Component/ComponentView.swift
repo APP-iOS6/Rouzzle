44e694b1882badb26b561e954befc8d0c6b73130
gadisom <jeongwon.kim@gmail.com>
2024-11-05 01:25:40 +0900

    Feat: RuzzleButton Component

---
+++ Rouzzle/Rouzzle/Utils/Component/ComponentView.swift
@@ -0,0 +1,18 @@
+//
+//  ComponentView.swift
+//  Rouzzle
+//
+//  Created by 김정원 on 11/5/24.
+//
+
+import SwiftUI
+
+struct ComponentView: View {
+    var body: some View {
+        RouzzleButton(buttonType: .save, action: {})
+    }
+}
+
+#Preview {
+    ComponentView()
+}
---
+++ Rouzzle/Rouzzle/Utils/Component/PuzzlePoint.swift
@@ -0,0 +1,8 @@
+//
+//  PuzzlePoint.swift
+//  Rouzzle
+//
+//  Created by 김정원 on 11/5/24.
+//
+
+import Foundation
