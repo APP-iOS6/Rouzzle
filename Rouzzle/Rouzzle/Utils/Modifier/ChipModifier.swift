91f71bd45770e1d20f872475beb90c720ae81053
kim dong kyung <kdkd422@naver.com>
2024-11-05 18:02:22 +0900

    Feat 칩버튼 모디파이어 추가

--- Rouzzle/Rouzzle/Utils/Modifier/ChipModifier.swift
+++ Rouzzle/Rouzzle/Utils/Modifier/ChipModifier.swift
@@ -5,4 +5,17 @@
 //  Created by 김동경 on 11/5/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ChipModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .padding(8)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(.gray, lineWidth: 1)
+            }
+    }
+}
