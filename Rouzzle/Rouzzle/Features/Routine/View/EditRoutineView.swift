114cf256feb96c5f37f18da3e90ba113fe00df6e
dardardardardar <180dayna@naver.com>
2024-11-07 22:21:41 +0900

    Feat: 루틴수정뷰 임시 연결

--- Rouzzle/Rouzzle/Features/Routine/View/EditRoutineView.swift
+++ Rouzzle/Rouzzle/Features/Routine/View/EditRoutineView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct EditRoutineView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text("루틴 수정 뷰")
+        VStack {
+            HStack {
+                Spacer()
+                Text("루틴 수정 뷰")
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                        .font(.semibold24)
+                }
+                .padding(.trailing, 20)
+            }
+            
+            Spacer()
+        }
     }
 }
 
