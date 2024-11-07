5dd329c9a783e1cf60bb494c442147a5ba264ab3
kim dong kyung <kdkd422@naver.com>
2024-11-07 22:02:11 +0900

    Chore project.pbxproj

---
+++ Rouzzle/Rouzzle/Features/Routine/ViewModel/NewTaskSheetViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  NewTaskSheetViewModel.swift
+//  Rouzzle
+//
+//  Created by 김동경 on 11/7/24.
+//
+
+import Factory
+import Foundation
+import Observation
+
+@Observable
+class NewTaskSheetViewModel {
+    
+    @ObservationIgnored
+    @Injected(\.routineService) private var routineService
+    
+    init() {
+        print("시트뷰모델")
+    }
+    
+    var emoji: String = ""
+    var text: String = ""
+    var hour: Int = 0
+    var min: Int = 0
+    var second: Int = 0
+    var errorMessage: String?
+    var sheetType: SheetType = .task
+}
+
+enum SheetType: Hashable {
+    case task
+    case time
+}
