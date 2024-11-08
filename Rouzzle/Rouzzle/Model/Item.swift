968c2b99d4ce4cc25bd18c49c172b68763327361
kim dong kyung <kdkd422@naver.com>
2024-11-08 13:48:28 +0900

    Refactor 파베 데이터 호환위해 init 수정

--- Rouzzle/Rouzzle/Model/Item.swift
+++ Rouzzle/Rouzzle/Model/Item.swift
@@ -10,25 +10,40 @@ import SwiftData
 
 @Model
 class RoutineItem: Identifiable {
-    var id = UUID()
+    var id: String = UUID().uuidString
     var title: String
     var emoji: String
     var repeatCount: Int?
     var interval: Int?
     var dayStartTime: [Int: Date]
     var alarmIDs: [Int: String]?
-
+    var userId: String
+    
     @Relationship(deleteRule: .cascade)
     var taskList: [TaskList] = []
-
+    
     init(
+        id: String,
         title: String,
         emoji: String,
-        dayStartTime: [Int: Date]
+        dayStartTime: [Int: Date],
+        repeatCount: Int?,
+        interval: Int?,
+        alarmIDs: [Int: String]?,
+        userId: String
     ) {
+        self.id = id
         self.title = title
         self.emoji = emoji
         self.dayStartTime = dayStartTime
+        self.repeatCount = repeatCount
+        self.interval = interval
+        self.alarmIDs = alarmIDs
+        self.userId = userId
+    }
+    
+    func toRoutine() -> Routine {
+        return Routine(documentId: id, title: title, emoji: emoji, routineTask: taskList.map { $0.toRoutineTask() }, dayStartTime: dayStartTime, userId: userId)
     }
 }
 
@@ -37,17 +52,23 @@ class TaskList: Identifiable {
     var id = UUID()
     var title: String
     var emoji: String
-    var timer: Int?
+    var timer: Int
     var isCompleted: Bool = false
-
+    
     @Relationship(inverse: \RoutineItem.taskList)
     var routineItem: RoutineItem?
-
+    
     init(
         title: String,
-        emoji: String
+        emoji: String,
+        timer: Int
     ) {
         self.title = title
         self.emoji = emoji
+        self.timer = timer
+    }
+    
+    func toRoutineTask() -> RoutineTask {
+        return RoutineTask(title: title, emoji: emoji, timer: timer)
     }
 }
