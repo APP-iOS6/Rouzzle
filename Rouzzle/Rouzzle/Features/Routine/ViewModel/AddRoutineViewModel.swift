955e543a7f39a4842b888c9e50e9fe83c60e3aa9
kim dong kyung <kdkd422@naver.com>
2024-11-08 13:47:12 +0900

    Feat 루틴 등록 뷰모델 생성

--- Rouzzle/Rouzzle/Features/Routine/ViewModel/AddRoutineViewModel.swift
+++ Rouzzle/Rouzzle/Features/Routine/ViewModel/AddRoutineViewModel.swift
@@ -5,4 +5,18 @@
 //  Created by 김동경 on 11/8/24.
 //
 
+import Factory
 import Foundation
+import Observation
+
+@Observable
+class AddRoutineViewModel {
+    
+    @ObservationIgnored
+    @Injected(\.routineService) private var routineService
+    
+    
+    func uploadRoutine() {
+        
+    }
+}
