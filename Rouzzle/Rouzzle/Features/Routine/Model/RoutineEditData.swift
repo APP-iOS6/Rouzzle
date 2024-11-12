fb6a9e5b4a2db27548184d6be56b7d8bf10e679e
kim dong kyung <kdkd422@naver.com>
2024-11-13 00:47:26 +0900

    Feat 루틴 수정에 사용하는 데이터 정의

--- Rouzzle/Rouzzle/Features/Routine/Model/RoutineEditData.swift
+++ Rouzzle/Rouzzle/Features/Routine/Model/RoutineEditData.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+struct RoutineEditData: Hashable {
+    var id: String
+    var title: String
+    var emoji: String
+    var repeatCount: Int?
+    var interval: Int?
+    var dayStartTime: [Int: String]
+    var alarmIDs: [Int: String]?
+    var userId: String
+    var taskList: [TaskEditData]
+    
+    func toRoutineItem() -> RoutineItem {
+        return RoutineItem(
+            id: id,
+            title: title,
+            emoji: emoji,
+            dayStartTime: dayStartTime,
+            repeatCount: repeatCount,
+            interval: interval,
+            alarmIDs: alarmIDs,
+            userId: userId
+        )
+    }
+}
+
+struct TaskEditData: Identifiable, Hashable {
+    var id: UUID
+    var title: String
+    var emoji: String
+    var timer: Int
+    var isCompleted: Bool
+    
+    func toTaskList() -> TaskList {
+        return TaskList(
+            id: id,
+            title: title,
+            emoji: emoji,
+            timer: timer,
+            isCompleted: isCompleted
+        )
+    }
+}
