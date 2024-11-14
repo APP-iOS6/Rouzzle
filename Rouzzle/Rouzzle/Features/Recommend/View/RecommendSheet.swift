fe0138617a012d70514e860e3e1a44cc29eb8f8d
kim dong kyung <kdkd422@naver.com>
2024-11-15 01:19:26 +0900

    Chore project.pbxproj

---
+++ Rouzzle/Rouzzle/Features/Recommend/View/RecommendSheet.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendSheet.swift
+//  Rouzzle
+//
+//  Created by 김동경 on 11/14/24.
+//
+
+import SwiftUI
+
+struct RecommendSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendSheet()
+}
