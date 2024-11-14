e6f1a00a565cb097651cde6c65f2afda775e108a
kim dong kyung <kdkd422@naver.com>
2024-11-15 01:22:05 +0900

    Feat 유명인 루틴 적용시 어느 루틴에 적용할 건지 고르는 시트 추가

--- Rouzzle/Rouzzle/Features/Recommend/View/RecommendSheet.swift
+++ Rouzzle/Rouzzle/Features/Recommend/View/RecommendSheet.swift
@@ -6,13 +6,60 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct RecommendSheet: View {
+    @Environment(\.dismiss) private var dismiss
+    @Query private var routines: [RoutineItem]
+    let tasks: [RecommendTodoTask]
+    let saveRoutine: (RoutineItem?) -> Void
+    @State private var selectedRoutineId: String?
+    var selectedRoutine: RoutineItem? {
+        routines.first(where: { $0.id == selectedRoutineId })
+    }
+    
+    init(
+        tasks: [RecommendTodoTask],
+        saveRoutine: @escaping (RoutineItem?) -> Void) {
+        self.tasks = tasks
+        self.saveRoutine = saveRoutine
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button {
+                    dismiss()
+                } label: {
+                    Text("닫기")
+                        .font(.semibold18)
+                }
+                Spacer()
+                Button {
+                    saveRoutine(selectedRoutine)
+                    dismiss()
+                } label: {
+                    Text("완료")
+                        .font(.semibold18)
+                }
+            }
+            .padding()
+            Picker(selection: $selectedRoutineId, label: Text("루틴 선택")) {
+                ForEach(routines) { routine in
+                    Text(routine.title).tag(Optional(routine.id))
+                        .font(.semibold20)
+                        .padding()
+                }
+                Text("루틴 추가하기").tag(nil as String?)
+            }
+            .pickerStyle(.wheel)
+        }
+        .onAppear {
+            selectedRoutineId = routines.first?.id ?? nil
+        }
     }
 }
 
 #Preview {
-    RecommendSheet()
+    RecommendSheet(tasks: []) { _ in}
 }
