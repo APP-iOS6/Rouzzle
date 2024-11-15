497f0238fa5102f2547f2333c594aa275ac69355
kim dong kyung <kdkd422@naver.com>
2024-11-15 17:59:22 +0900

    Feat 테두리 버튼 모디파이어 추가

--- Rouzzle/Rouzzle/Utils/Modifier/BorderButtonModifier.swift
+++ Rouzzle/Rouzzle/Utils/Modifier/BorderButtonModifier.swift
@@ -5,4 +5,18 @@
 //  Created by 김동경 on 11/15/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct BorderButtonModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .frame(height: 61)
+            .frame(maxWidth: .infinity)
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(.accent, lineWidth: 1)
+            }
+    }
+}
