f4c3e5eeaf80d3b8f4a885e34c1299e083955715
kim dong kyung <kdkd422@naver.com>
2024-11-15 13:45:23 +0900

    Chore project.pbxproj

---
+++ Rouzzle/Rouzzle/Features/Routine/View/AddRoutineContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  AddRoutineContainerView.swift
+//  Rouzzle
+//
+//  Created by 김동경 on 11/15/24.
+//
+
+import SwiftUI
+
+struct AddRoutineContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddRoutineContainerView()
+}
---
+++ Rouzzle/Rouzzle/Features/Routine/View/AddRoutineTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddRoutineTaskView.swift
+//  Rouzzle
+//
+//  Created by 김동경 on 11/15/24.
+//
+
+import SwiftUI
+
+struct AddRoutineTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddRoutineTaskView()
+}
