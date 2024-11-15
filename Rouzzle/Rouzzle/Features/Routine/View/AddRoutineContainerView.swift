a03139d4021d16ba8b0e43e13980b08b53d82af6
kim dong kyung <kdkd422@naver.com>
2024-11-15 13:45:50 +0900

    Feat 루틴 생성 컨테이너 뷰

--- Rouzzle/Rouzzle/Features/Routine/View/AddRoutineContainerView.swift
+++ Rouzzle/Rouzzle/Features/Routine/View/AddRoutineContainerView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct AddRoutineContainerView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @State private var viewModel: AddRoutineViewModel = .init()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            HStack {
+                Button {
+                   goBack()
+                } label: {
+                    Image(systemName: viewModel.step == .info ? "xmark" : "chevron.left")
+                        .font(.semibold18)
+                }
+                Spacer()
+            }
+            .overlay {
+                Text(viewModel.step == .info ? "루틴 등록" : "할 일 등록")
+                    .font(.semibold18)
+                
+            }
+            .padding()
+            
+            ProgressView(value: viewModel.step.rawValue, total: 1.0)
+                .tint(.accent)
+                .progressViewStyle(.linear)
+                .padding(.horizontal)
+            
+            switch viewModel.step {
+            case .info:
+                AddRoutineView(viewModel: viewModel)
+            case .task:
+                AddRoutineTaskView(viewModel: viewModel)
+            }
+            Spacer()
+        }
+        .animation(.easeInOut(duration: 0.1), value: viewModel.step)
+    }
+    private func goBack() {
+        switch viewModel.step {
+        case .info:
+            dismiss()
+        case .task:
+            viewModel.step = .info
+        }
     }
 }
 
