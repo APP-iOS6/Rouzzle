c02fd623a934316561c110a65aa15d47b47d03c8
Hyeonjeong-Sim <56778111@naver.com>
2024-11-15 10:37:07 +0900

    fix: 이미지,버튼 클릭시 퍼즐뷰 연결

--- Rouzzle/Rouzzle/Features/RouzzleChallenge/View/RouzzleChallengePuzzleView.swift
+++ Rouzzle/Rouzzle/Features/RouzzleChallenge/View/RouzzleChallengePuzzleView.swift
@@ -9,31 +9,10 @@ import SwiftUI
 
 struct RouzzleChallengePuzzleView: View {
     var body: some View {
-        ZStack(alignment: .top) {
-            LinearGradient(
-                colors: [.white, Color.fromRGB(r: 252, g: 255, b: 240)],
-                startPoint: .top,
-                endPoint: .bottom
-            )
-            .ignoresSafeArea()
-            
-            ScrollView(showsIndicators: false) {
-                // 여기에 퍼즐 콘텐츠를 추가할 수 있습니다
-                Text("퍼즐 콘텐츠가 들어갈 자리입니다")
-                    .padding()
-            }
-            .customNavigationBar(title: "루즐 퍼즐테스트")
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    PieceCounter(count: 9)
-                }
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    NavigationStack {
-        RouzzleChallengePuzzleView()
-    }
+    RouzzleChallengePuzzleView()
 }
