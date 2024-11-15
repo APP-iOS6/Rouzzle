a84e3b9a4e919cf105dc9c748264ec19264eada3
Hyeonjeong-Sim <56778111@naver.com>
2024-11-15 10:52:43 +0900

    feat: PuzzleGame 클래스 추가

--- Rouzzle/Rouzzle/Features/RouzzleChallenge/View/PuzzleGame.swift
+++ Rouzzle/Rouzzle/Features/RouzzleChallenge/View/PuzzleGame.swift
@@ -5,4 +5,45 @@
 //  Created by Hyeonjeong Sim on 11/15/24.
 //
 
-import Foundation
+import SwiftUI
+
+@Observable
+class PuzzleGame {
+    var puzzlePieceCount: Int
+    var showNoPiecesAlert: Bool = false
+    var isCompleted: Bool = false
+    
+    let puzzleType: PuzzleType
+    
+    init(puzzleType: PuzzleType, initialPieceCount: Int = 100) {
+        self.puzzleType = puzzleType
+        self.puzzlePieceCount = initialPieceCount
+    }
+    
+    func usePuzzlePiece() -> Bool {
+        if puzzlePieceCount > 0 {
+            puzzlePieceCount -= 1
+            return true
+        } else {
+            showNoPiecesAlert = true
+            return false
+        }
+    }
+}
+
+enum PuzzleType {
+    case tuna
+    case ned
+    case chan
+    
+    var imageName: String {
+        switch self {
+        case .tuna:
+            return "tunapuzzle"
+        case .ned:
+            return "tunapuzzle" // 임시로 tuna 이미지 사용
+        case .chan:
+            return "tunapuzzle" // 임시로 tuna 이미지 사용
+        }
+    }
+}
