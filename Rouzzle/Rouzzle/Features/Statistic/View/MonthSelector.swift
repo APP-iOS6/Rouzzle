1e854f6e00a9594d57c31367c4b53a8d192687af
Hyeonjeong-Sim <56778111@naver.com>
2024-11-16 14:57:08 +0900

    feat: 스위프트차트 추가, 나의 루틴 목록 데이터 가져오는 거 진행중

---
+++ Rouzzle/Rouzzle/Features/Statistic/View/MonthSelector.swift
@@ -0,0 +1,18 @@
+//
+//  MonthSelector.swift
+//  Rouzzle
+//
+//  Created by Hyeonjeong Sim on 11/16/24.
+//
+
+import SwiftUI
+
+struct MonthSelector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MonthSelector()
+}
---
+++ Rouzzle/Rouzzle/Features/Statistic/View/RoutineSuccessRateChart.swift
@@ -0,0 +1,18 @@
+//
+//  RoutineSuccessRateChart.swift
+//  Rouzzle
+//
+//  Created by Hyeonjeong Sim on 11/16/24.
+//
+
+import SwiftUI
+
+struct RoutineSuccessRateChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoutineSuccessRateChart()
+}
