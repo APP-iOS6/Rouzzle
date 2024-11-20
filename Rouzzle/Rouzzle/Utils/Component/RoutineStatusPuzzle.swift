b9b6aad43113c5bb61d49a8f0e465f26a52a1ace
dardardardardar <180dayna@naver.com>
2024-11-21 02:23:11 +0900

    [Feat]: 루틴 알람 on/off에 따른 bell이미지 변경주기 (addView만 연결)

--- Rouzzle/Rouzzle/Utils/Component/RoutineStatusPuzzle.swift
+++ Rouzzle/Rouzzle/Utils/Component/RoutineStatusPuzzle.swift
@@ -26,31 +26,34 @@ struct RoutineStatusPuzzle: View {
     
     @Bindable var routineItem: RoutineItem
     @Environment(\.modelContext) private var modelContext
+    
     var todayStartTime: String {
         let today = Date()
         let calendar = Calendar.current
         let weekdayNumber = calendar.component(.weekday, from: today)
         let value = routineItem.dayStartTime[weekdayNumber] ?? routineItem.dayStartTime.first?.value ?? ""
-        return value.to12HourPeriod() + " " +  value.to12HourFormattedTime()
+        return value.to12HourPeriod() + " " + value.to12HourFormattedTime()
     }
+    
     var status: RoutineStatus {
-        return ((routineItem.taskList.filter {$0.isCompleted}.count == routineItem.taskList.count) && !routineItem.taskList.isEmpty) ? .completed : .pending
+        return ((routineItem.taskList.filter { $0.isCompleted }.count == routineItem.taskList.count) && !routineItem.taskList.isEmpty) ? .completed : .pending
     }
     
     var inProgressStr: String {
-        let completedTaskCount = routineItem.taskList.filter {$0.isCompleted}.count
+        let completedTaskCount = routineItem.taskList.filter { $0.isCompleted }.count
         if completedTaskCount == 0 {
             return ""
         }
         return "\(completedTaskCount)/\(routineItem.taskList.count)"
     }
     
-    var alramImageName: String {
-        routineItem.alarmIDs == nil ? "bell.slash" : "bell"
-    }
-    
-    var isAlram: Bool {
-        routineItem.alarmIDs == nil ? false : true
+    var alarmImageName: String {
+        // 알림 ID가 존재하고 값이 비어있지 않은 경우 알림 활성화로 표시
+        if let alarmIDs = routineItem.alarmIDs, !alarmIDs.isEmpty {
+            return "bell"
+        } else {
+            return "bell.slash"
+        }
     }
     
     var body: some View {
@@ -58,7 +61,7 @@ struct RoutineStatusPuzzle: View {
             status.image
                 .resizable()
                 .frame(maxWidth: .infinity)
-                .aspectRatio(370/137, contentMode: .fit)
+                .aspectRatio(370 / 137, contentMode: .fit)
             
             HStack {
                 Text("\(routineItem.emoji)")
@@ -81,11 +84,11 @@ struct RoutineStatusPuzzle: View {
                 
                 VStack(alignment: .trailing, spacing: 5) {
                     HStack(spacing: 5) {
-                        Image(systemName: isAlram ? "bell" : "bell.slash")
+                        Image(systemName: alarmImageName) // 알림 이미지 동적으로 업데이트
                         Text(todayStartTime)
                     }
                     .font(.regular16)
-                    Text(convertDaysToStirng(days: routineItem.dayStartTime.keys.sorted()))
+                    Text(convertDaysToString(days: routineItem.dayStartTime.keys.sorted()))
                         .font(.regular14)
                 }
                 .foregroundStyle(Color.subHeadlineFontColor)
@@ -96,15 +99,15 @@ struct RoutineStatusPuzzle: View {
         .opacity(status == .pending ? 1 : 0.6)
     }
     
-    func convertDaysToStirng(days: [Int]) -> String {
+    func convertDaysToString(days: [Int]) -> String {
         var str = ""
         for day in days {
             str += " \(dayOfWeek[day, default: ""])"
         }
         return str
     }
-    
 }
+
 
 // #Preview("2") {
 //    RoutineStatusPuzzle2()
