2941a13e2d5b8e93f1d512ebf52f4d5b6e81fcce
kim dong kyung <kdkd422@naver.com>
2024-11-20 16:44:22 +0900

    Chore projext.pbxproj

---
+++ Rouzzle/Rouzzle/Features/Statistic/View/RoutineStatisticView.swift
@@ -0,0 +1,18 @@
+//
+//  RoutineStatisticView.swift
+//  Rouzzle
+//
+//  Created by 김동경 on 11/20/24.
+//
+
+import SwiftUI
+
+struct RoutineStatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoutineStatisticView()
+}
---
+++ Rouzzle/Rouzzle/Model/RoutineStatistic.swift
@@ -0,0 +1,18 @@
+//
+//  RoutineStatistic.swift
+//  Rouzzle
+//
+//  Created by 김동경 on 11/20/24.
+//
+
+import SwiftUI
+
+struct RoutineStatistic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoutineStatistic()
+}
