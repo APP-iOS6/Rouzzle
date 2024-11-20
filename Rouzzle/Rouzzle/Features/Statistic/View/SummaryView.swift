2f353a365b925f5ef1471a98903c61ce110aa6e7
kim dong kyung <kdkd422@naver.com>
2024-11-20 10:26:12 +0900

    Chore projext.pbxproj

--- Rouzzle/Rouzzle/Features/Statistic/View/SummaryView.swift
+++ Rouzzle/Rouzzle/Features/Statistic/View/SummaryView.swift
@@ -1,66 +1,58 @@
 //
-//  SummaryView.swift
+//  SummaryView2.swift
 //  Rouzzle
 //
-//  Created by Hyeonjeong Sim on 11/13/24.
+//  Created by 김동경 on 11/19/24.
 //
 
 import SwiftUI
-import Charts
 
-struct SummaryView: View {
-    let viewModel: StatisticViewModel
+struct SummaryView2: View {
+    
+    @ObservedObject var store: StatisticStore
+    let routines: [RoutineItem]
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            // 최대 연속기록 박스
+        VStack(spacing: 20) {
             HStack {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("나의 최대 연속 기록이에요!")
-                        .font(.medium16)
-                    HStack(alignment: .bottom, spacing: 8) {
-                        Text("\(viewModel.getMaxConsecutiveDays())일")
-                            .font(.bold36)
-                        Text(viewModel.getMaxConsecutiveRoutineName())
-                            .font(.regular16)
-                            .foregroundStyle(.gray)
-                            .alignmentGuide(.bottom) { $0[.bottom] + 4 }
-                        Spacer()
-                    }
-                }
-                .padding()
-                .frame(height: 107)
-                .frame(maxWidth: .infinity)
-                .background(
-                    RoundedRectangle(cornerRadius: 12)
-                        .fill(Color.backgroundLightGray)
-                )
-            }
-            .padding(.bottom, 20)
-            
-            HStack {
+                
                 Text("월간 성공률")
                     .font(.bold16)
                 
                 Spacer()
                 
-                MonthSelector(viewModel: viewModel)
-            }
-            
-            // 루틴별 성공률 차트
-            VStack(spacing: 8) {
-                ForEach(viewModel.routines) { routine in
-                    RoutineSuccessRateChart(routine: routine, viewModel: viewModel)
+                MonthSelector2(
+                    title: store.currentDate.extraData,
+                    isLoading: $store.isLoading
+                ) { value in
+                    store.moveMonth(direction: value)
                 }
             }
-            .padding(.vertical, 12)
-            .padding(.leading, -25)
+            VStack {
+                ForEach(routines, id: \.id) { routine in
+                    let targetDay: Set<Int> = Set(routine.dayStartTime.keys)
+                    let percentage: Double = Double(store.summaryData[routine.id]?.filter { $0.isCompleted }.count ?? 0) / Double(store.countMTTDays(targetDay)) * 100
+                    
+                    RoutineSuccessRateChart2(
+                        percentage: percentage,
+                        emoji: routine.emoji,
+                        title: routine.title
+                    )
+                    .padding(.vertical, 12)
+                }
+                Spacer()
+                    .frame(height: 12)
+            }
+            .padding(.horizontal)
             .frame(maxWidth: .infinity)
             .background(
                 RoundedRectangle(cornerRadius: 12)
                     .fill(Color.backgroundLightGray)
             )
         }
-        .frame(maxWidth: .infinity)
     }
+}
+
+#Preview {
+    SummaryView2(store: .init(), routines: [])
 }
---
+++ Rouzzle/Rouzzle/Store/StatisticStore.swift
@@ -0,0 +1,14 @@
+//
+//  StatisticStore.swift
+//  Rouzzle
+//
+//  Created by 김동경 on 11/18/24.
+//
+
+import Foundation
+import Observation
+
+@Observable
+class StatisticStore {
+    
+}
