cfc804b990e70c4048118139e825b4a94e2ff99f
kim dong kyung <kdkd422@naver.com>
2024-11-25 17:51:41 +0900

    Home 퍼즐 Shop 연결

--- Rouzzle/Rouzzle/Utils/Component/PieceCounter.swift
+++ Rouzzle/Rouzzle/Utils/Component/PieceCounter.swift
@@ -61,22 +61,26 @@ struct PieceCounter: View {
     }
     
     private var counterContent: some View {
-        HStack {
-            Image(.piece)
-                .resizable()
-                .scaledToFit()
-                .frame(height: 18)
-            
-            Text("\(count)")
-                .font(.medium16)
-                .foregroundStyle(.black)
+        NavigationLink {
+            ShopView()
+        } label: {
+            HStack {
+                Image(.piece)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 18)
+                
+                Text("\(count)")
+                    .font(.medium16)
+                    .foregroundStyle(.black)
+            }
+            .frame(width: 70, height: 30)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.white)
+                    .shadow(color: .black.opacity(0.1), radius: 2)
+            )
         }
-        .frame(width: 70, height: 30)
-        .background(
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.white)
-                .shadow(color: .black.opacity(0.1), radius: 2)
-        )
     }
 }
 
